2ca09c5fb7707d8eaf40edede7a1b49d1a3ce18d
Taha Khalil Charif <taha.khalil018@gmail.com>
2024-11-14 23:00:04 +0100

    heaps

---
+++ include/heap.h
@@ -0,0 +1,18 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+typedef struct {
+    int *data;      
+    int size;       
+    int capacity;  
+} MinHeap;
+
+MinHeap* createHeap(int capacity);
+void insertMinHeap(MinHeap* heap, int value);
+int extractMin(MinHeap* heap);
+void minHeapify(MinHeap* heap, int index);
+void buildMinHeap_NlogN(MinHeap* heap, int* array, int n);
+void buildMinHeap_N(MinHeap* heap, int* array, int n);
+void freeHeap(MinHeap* heap);
+
+#endif
